74e9ebbd07a985dd6cd8231325578f8b9699a799
josevmarques <josev.marques01@gmail.com>
2023-11-13 00:43:01 -0300

    feat: Tela de login

---
+++ frontend/src/views/login/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Lógica de autenticação aqui
+        if (username === 'admin' && password === 'admin') {
+            alert('Login bem-sucedido!');
+        } else {
+            alert('Credenciais inválidas. Tente novamente.');
+        }
+    };
+
+    return (
+        <div className="container container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-md-4">
+                    <div className="card mt-5">
+                        <div className="card-body">
+                            <h1 className="card-title text-center">Entrar</h1>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nome de usuário"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Senha"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <button className="btn btn-primary btn-block" onClick={handleLogin}>
+                                Entrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
