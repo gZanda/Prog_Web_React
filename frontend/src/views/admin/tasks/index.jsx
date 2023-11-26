8959cc53be0b4ddd2b699d13856d4a54116943d6
Otávio <otavio.rodf@gmail.com>
2023-11-26 18:29:06 -0300

    feat: implemented api integration for tasks and fixed some users work arounds

--- frontend/src/views/admin/tasks/index.jsx
+++ frontend/src/views/admin/tasks/index.jsx
@@ -1,46 +1,131 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from '../../../utils/axios_instance';
 
 export default function Tasks () {
-    let v_Data = [
-        {
-            
-        }
-    ]
+    const [data, setData] = useState([]);
+		const [idTask, setIdTask] = useState();
+		const [permissionDenied, setPermissionDenied] = useState(false);
+    
+		// Faz a request toda vez que o componente é renderizado em tela (Primeira vez que abre a página ou cada vez que atualiza a pagina)
+		useEffect(() =>{
+			async function fetch() {
+				try{
+					const response = await axios.get('/tasks/')
+					setData(response.data)
+					console.log(data)
+				}catch(error){
+					if (error.response.status === 403) setPermissionDenied(true);
+					alert('Ocorreu um erro:', error);
+				}
+			}
+
+			fetch();
+    }, [Tasks])
+
+		// função para retornar uma task apartir do seu ID *ESTILIZAR*
+		async function retrieveTask(){
+			try{
+				const response = await axios.get(`/tasks/${idTask}/`);
+				setData([response.data])
+			}catch(error){
+				if (error.response.status === 403) permissionDenied(true);
+				alert('Ocorreu um erro: ', error);
+				console.log(error)
+			}
+		}
+
+		//função para criar task (os dados estão estáticos, tem que mudar o objeto mandado na requisição pros dados do formulário) *ESTILIZAR E ABRIR FORMULÁRIO (MODAL?)* 
+		async function addTask(){
+			try{
+				const response = await axios.post("/createTask/", 
+					{
+						"description":"Fazer Trabalho",
+						"status":"Pendente",
+						"approval_status":"Não Avaliada",
+						"responsible": 1
+					}
+					)
+				console.log(response.data)
+			}catch(error){
+				if(error.response.status === 403) setPermissionDenied(true);
+				alert("Ocorreu um erro: ", error)
+			}
+		}
+		//função para editar task (os dados estão estáticos, tem que mudar o objeto mandado na requisição pros dados do formulário)*ESTILIZAR*
+		async function editTask(){
+			try{
+				const response = await axios.put(`/editTask/${idTask}/`, 
+				{
+					"description":"Task editada!",
+					"status":"Pendente",
+					"approval_status":"Não Avaliada",
+					"responsible": 2
+				}
+				);
+				setData([response.data])
+			}catch(error){
+				if (error.response.status === 403) permissionDenied(true);
+				alert('Ocorreu um erro: ', error);
+				console.log(error)
+			}
+		}
+
+		//função para deletar task *ESTILIZAR com um botão 'X'* 
+		async function deleteTask(){
+			try{
+				const response = await axios.delete(`/deleteTask/${idTask}/`);
+				setData([response.data])
+			}catch(error){
+				if (error.response.status === 403) permissionDenied(true);
+				alert('Ocorreu um erro: ', error);
+				console.log(error)
+			}
+		}
+
+		if (permissionDenied){
+			return(
+				<>
+					<h1> PERMISSÃO NEGADA</h1>
+					<b> Essa funcionalidade requer o cargo de manager </b>
+				</> 
+			)
+		}
 
     return (
         <div>
-            <button className={"py-2 mb-4 px-4 border rounded-md text-white bg-blue-600"}>
+            <button className={"py-2 mb-4 px-4 border rounded-md text-white bg-blue-600"} onClick={addTask}>
                 Add record
             </button>
+
+						<input type="number" name="id_task" onChange={(e) => setIdTask(e.target.value)}/>
+						<button onClick={retrieveTask} > Retornar Task </button>
+
+						<input type="number" name="id_task" onChange={(e) => setIdTask(e.target.value)}/>
+						<button onClick={deleteTask} > Deletar Task </button>
+						
+						<input type="number" name="id_task" onChange={(e) => setIdTask(e.target.value)}/>
+						<button onClick={editTask} > Editar Task </button>
 
             <table className="table">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Descrição</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Status de aprovação</th>
                         <th scope="col">Responsável</th>
-                        <th scope="col">Data</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+									{data.map(el => (
+											<tr>
+													<th scope="row"> {el.id}</th>
+													<td>{el.description}</td>
+													<td>{el.status}</td>
+													<td>{el.approval_status}</td>
+													<td>{el.responsible}</td>
+										</tr>
+									))}
                 </tbody>
             </table>
         </div>
--- frontend/src/views/admin/users/index.jsx
+++ frontend/src/views/admin/users/index.jsx
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from '../../../utils/axios_instance';
 import './styles.css';
 
 export default function Users(){
   const [permissionDenied, setPermissionDenied] = useState(false);
   const [data, setData] = useState([]);
+  const [idUser, setIdUser] = useState();
 
-  async function handleRequest() {
+  
+  // Faz a request toda vez que o componente é renderizado em tela (Primeira vez que abre a página ou cada vez que atualiza a pagina) *ESTILIZAR*
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get('/users/');
+        setData(response.data)
+      }catch(error){
+        if (error.response.status === 403) setPermissionDenied(true);
+        alert("Ocorreu um erro:", error)
+      }
+    }
+    fetchUsers();
+  }, [])
+
+  //pega um usuário a partir do id de um input *ESTILIZAR*
+  async function getUser(){
     try {
-      const response = await axios.get('/users/');
-      setData(response.data)
+      const response = await axios.get(`/users/${idUser}/`);
+      setData([response.data])
+    }catch(error){
+      if (error.response.status === 403) setPermissionDenied(true);
+      alert("Ocorreu um erro:", error)
+    }
+  }
+
+  //deleta um usuário a partir do id *ESTILIZAR e fazer o delete por um botão 'X' talvez? *
+  async function deleteUser(){
+    try {
+      const response = await axios.delete(`/deleteUser/${idUser}/`);
     }catch(error){
       if (error.response.status === 403) setPermissionDenied(true);
       alert("Ocorreu um erro:", error)
@@ -29,14 +56,20 @@ export default function Users(){
   return(
     <>
       <h1>RETORNA OS DADOS DE USUÁRIOS AQUI</h1>
-      <button onClick={handleRequest}>Retornar dados</button>
+      
+      <input type="number" name="id_usuarios" onChange={(e) => setIdUser(e.target.value)}/>
+      <button onClick={getUser}>Retornar usuário</button>
+
+      <input type="number" name="delete_usuarios" onChange={(e) => setIdUser(e.target.value)}/>
+      <button onClick={deleteUser}>Deletar um usuário por id</button>
+
       <ul>
-        <h1>{data.map(el => (
+        {data.map(el => (
           <>
             <li>{el.username}</li>
             <li>{el.email}</li>
           </>
-        ))}</h1>
+        ))}
       </ul>
     </>
     
